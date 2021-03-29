afdacbac7bf66c42bdc3bcab4f097315021c7443
ivmartel <ivmartel@gmail.com>
2021-03-29 18:47:20 +0200

    Update page title and version.

--- src/applauncher.js
+++ src/applauncher.js
@@ -11,7 +11,7 @@ function startApp() {
   dwvjq.gui.setup();
 
   // show dwv version
-  dwvjq.gui.appendVersionHtml(dwv.getVersion());
+  dwvjq.gui.appendVersionHtml('0.5.0');
 
   // application options
   var filterList = ['Threshold', 'Sharpen', 'Sobel'];
