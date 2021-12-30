d1aa0f4918396f758503621daff9c2d10f3dec4e
ivmartel <ivmartel@gmail.com>
2021-12-30 16:31:03 +0100

    Bump version from 0.6.0 to 0.7.0-beta

--- src/applauncher.js
+++ src/applauncher.js
@@ -11,7 +11,7 @@ function startApp() {
   dwvjq.gui.setup();
 
   // show dwv version
-  dwvjq.gui.appendVersionHtml('0.6.0');
+  dwvjq.gui.appendVersionHtml('0.7.0-beta');
 
   // application options
   var filterList = ['Threshold', 'Sharpen', 'Sobel'];
